b9ff6150e61fca1e242f6e4c97891520eaa9ac96
Matt Pye <pyematt@gmail.com>
2014-10-26 04:00:44 +0000

    Setting url max_lengths

--- bandnames/bandnames/names/models.py
+++ bandnames/bandnames/names/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Bands(models.Model):
     name = models.CharField(max_length=100)
     reason = models.CharField(max_length=3000)
-    source = models.CharField(max_length=20)
-    scrapped_from = models.CharField(max_length=50)
+    source = models.CharField(max_length=200)
+    scrapped_from = models.CharField(max_length=200)
 
     def __unicode__(self):
         return u"{name}".format(name=self.name)
