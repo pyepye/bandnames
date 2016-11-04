f4f73c74a9c2b6fb974292fd583bcd18bbc694ae
Matt Pye <pyematt@gmail.com>
2016-11-04 22:47:47 +0000

    Clean up settings

--- bandnames/bandnames/settings/production.py
+++ bandnames/bandnames/settings/production.py
@@ -5,82 +5,37 @@ from __future__ import absolute_import
 import os
 
 import dj_database_url
-from django.core.exceptions import ImproperlyConfigured
 
 from .base import *
-
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-
-
-def get_env_setting(setting):
-    """ Get the os.environment setting or return exception """
-    try:
-        return os.environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
-
 
 STATICFILES_DIRS = (
     normpath(join(SITE_ROOT, 'static')),
 )
 
-########## HOST CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 ALLOWED_HOSTS = [
     '.herokuapp.com', '192.168.1.100', '127.0.0.1', 'bandnames.mattpye.com'
 ]
-########## END HOST CONFIGURATION
-
-########## EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'your_email@example.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
 EMAIL_USE_TLS = True
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = EMAIL_HOST_USER
-########## END EMAIL CONFIGURATION
 
-########## DATABASE CONFIGURATION
-# Parse database configuration from $DATABASE_URL
+
 DATABASES['default'] = dj_database_url.config()
-########## END DATABASE CONFIGURATION
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
 DEFAULT_CACHE_ALIAS = 'default'
-########## END CACHE CONFIGURATION
 
-########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('DJANGO_SECRET_KEY')
-########## END SECRET CONFIGURATION
 
 LOGGING = {
     "version": 1,
@@ -98,7 +53,6 @@ LOGGING = {
     }
 }
 
-########## DATABASE CONFIGURATION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -109,4 +63,3 @@ DATABASES = {
         'PORT': '',
     }
 }
-########## END DATABASE CONFIGURATION
--- bandnames/bandnames/settings/test.py
+++ bandnames/bandnames/settings/test.py
@@ -2,12 +2,11 @@ from __future__ import absolute_import
 
 from .base import *
 
-########## TEST SETTINGS
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
-########## IN-MEMORY TEST DATABASE
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
