969adce4858c61ebc7275a4239dd3c7697f15ee4
Matt Pye <pyematt@gmail.com>
2017-05-18 01:00:45 +0100

    Fix up form pages

--- bandnames/bandnames/names/forms.py
+++ bandnames/bandnames/names/forms.py
@@ -5,22 +5,31 @@ from bandnames.names.models import NewBand, ReportBand
 
 
 class ReportBandForm(forms.ModelForm):
-    reason = forms.CharField(widget=forms.Textarea)
-    source = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'A link of proof will help us varify'
-    }))
-    reporter_name = forms.CharField(
-        label='Name / Alias',
-        required=False,
-        widget=forms.TextInput(attrs={
-            'placeholder': '(Optional) So we can give you all the credit'
+    reason = forms.CharField(
+        label='Reason (how did they actually get their name?)  *',
+        widget=forms.Textarea(attrs={
+            'class': 'mdl-textfield__input',
+            'rows': '7',
         })
     )
-    reporter_email = forms.CharField(
-        label='Email',
+    source = forms.CharField(
+        label='Source (URL)  *',
+        widget=forms.TextInput(attrs={
+            'class': 'mdl-textfield__input',
+        })
+    )
+    reporter_name = forms.CharField(
+        label='Your Name / Alias',
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': '(Optional) In case we need to get in touch'
+            'class': 'mdl-textfield__input',
+        })
+    )
+    reporter_email = forms.EmailField(
+        label='Your Email (in case we need to get in touch)',
+        required=False,
+        widget=forms.EmailInput(attrs={
+            'class': 'mdl-textfield__input',
         })
     )
 
@@ -30,23 +39,37 @@ class ReportBandForm(forms.ModelForm):
 
 
 class NewBandForm(forms.ModelForm):
-    name = forms.CharField(label='Band Name')
-    reason = forms.CharField(widget=forms.Textarea)
-    source = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'A link of proof will help us varify'
-    }))
-    submitter_name = forms.CharField(
-        label='Name / Alias',
-        required=False,
+    name = forms.CharField(
+        label='The bands name  *',
         widget=forms.TextInput(attrs={
-            'placeholder': '(Optional) So we can give you all the credit'
+            'class': 'mdl-textfield__input',
         })
     )
-    submitter_email = forms.CharField(
-        label='Email',
+    reason = forms.CharField(
+        label='Reason (how did they get their name?)  *',
+        widget=forms.Textarea(attrs={
+            'class': 'mdl-textfield__input',
+            'rows': '7',
+        })
+    )
+    source = forms.CharField(
+        label='Source (URL)  *',
+        widget=forms.TextInput(attrs={
+            'class': 'mdl-textfield__input',
+        })
+    )
+    submitter_name = forms.CharField(
+        label='Your Name / Alias (so you can get credit)',
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': '(Optional) In case we need to get in touch'
+            'class': 'mdl-textfield__input',
+        })
+    )
+    submitter_email = forms.EmailField(
+        label='Your Email (so we can notify you when it\' approved)',
+        required=False,
+        widget=forms.EmailInput(attrs={
+            'class': 'mdl-textfield__input',
         })
     )
 
